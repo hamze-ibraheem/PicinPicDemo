a32669679fa089e3c6141c9cc442a1b4ee903f39
Hamza Al-Sarsour <hamze.85@live.com>
2021-05-31 15:47:35 +0300

    finish the app demo.

--- app/src/main/java/com/taskfoundation/picinpicdemo/MainActivity.java
+++ app/src/main/java/com/taskfoundation/picinpicdemo/MainActivity.java
@@ -2,7 +2,10 @@ package com.taskfoundation.picinpicdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goPicInPicMode(View view) {
+        enterPictureInPictureMode();
+    }
+
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
+        TextView textView = findViewById(R.id.textView);
+        if (isInPictureInPictureMode) {
+            getSupportActionBar().hide();
+            findViewById(R.id.button).setVisibility(View.GONE);
+            textView.setText("PIP MODE");
+        } else {
+            getSupportActionBar().show();
+            findViewById(R.id.button).setVisibility(View.VISIBLE);
+            textView.setText("HELLO WORLD!");
+        }
     }
 }
